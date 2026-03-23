f1838074e07bd3dced7c702cb154616e5987e8f4
mnjese <mjk5949@naver.com>
2026-03-23 17:48:42 +0900

    [feat] 기본 모드를 다크에서 라이트로 변경

--- src/hooks/useTheme.tsx
+++ src/hooks/useTheme.tsx
@@ -16,7 +16,7 @@ function getInitialTheme(): Theme {
     return storedTheme
   }
 
-  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+  return 'light'
 }
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
